c7abff3541cf053e785a7af7862d42e55e82ddcb
rajesh kumar <rajesh.kumar@bchydro.com>
2024-01-11 22:05:18 -0800

    feat(player): Logo adjustment; Player autoplay disabled and unmuted

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -9,7 +9,7 @@ export const Logo: React.FunctionComponent<Header> = () => {
   // const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box w="100%">
-      <Link to="/"><Image m="auto" w="200px" src={logo} /></Link>
+      <Link to="/"><Image m="auto" w="200px" paddingTop={"0.2rem"} src={logo} /></Link>
     </Box>
   );
 };
