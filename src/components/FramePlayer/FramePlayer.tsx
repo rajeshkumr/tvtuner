d99d917247881579e1351ff889744c7733e0b8eb
rajesh kumar <rajesh.kumar@bchydro.com>
2024-03-10 18:44:53 -0700

    feat: oscar 2024 stream added

--- src/components/FramePlayer/FramePlayer.tsx
+++ src/components/FramePlayer/FramePlayer.tsx
@@ -2,6 +2,6 @@
 
 export const FramePlayer: React.FunctionComponent = () => {
   return (
-  <iframe src={"https://stream.crichd.vip/update/star1hi.php"} style={{aspectRatio: 16/9}} width="100%" height="100%" allow="autoplay; fullscreen; picture-in-picture" /> 
+  <iframe src={"https://awardstreams.netlify.app/jwplayer"} style={{aspectRatio: 16/9}} width="100%" height="100%" allow="autoplay; fullscreen; picture-in-picture" /> 
   );
 };
