fdded1d7cbed4e9c54f3473dcc74f0702bcb033b
rajesh kumar <rajesh.kumar@bchydro.com>
2024-03-11 10:00:31 -0700

    feat: new url with auth key added

--- src/components/FramePlayer/FramePlayer.tsx
+++ src/components/FramePlayer/FramePlayer.tsx
@@ -5,7 +5,7 @@ export const FramePlayer: React.FunctionComponent = () => {
   // <iframe src={"https://thetvapp.to/tv/wabc-new-york-abc-east-live-stream/"} style={{aspectRatio: 16/9}} width="100%" height="100%" allow="autoplay; fullscreen; picture-in-picture" /> 
   // );
   <ReactPlayer
-          url={"https://content-auso1.uplynk.com/channel/640a5a594b434c2691e20580afda564f/g.m3u8?pbs=258490b109aa418b9ec69ecb668bc74f"}
+          url={"https://content-auso1.uplynk.com/channel/640a5a594b434c2691e20580afda564f/g.m3u8?pbs=7c562f71727a4a088dc14f3712866c24"}
           width={"100%"}
           height={"100%"}
           playing={true}
